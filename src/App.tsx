4d7df05c0ad33aa2c02fcfa5adfb346b8bd0f2a6
Yebin Yu <yuyaebean@gmail.com>
2021-03-26 23:43:54 +0900

    Feat: Set normalize css & background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import styled from '@emotion/styled';
 import Numpads from 'components/Numpads';
+
+const FullScreen = styled.div`
+  background: black;
+  width: 100vw;
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <>
+    <FullScreen>
       <Numpads />
-    </>
+    </FullScreen>
   );
 }
 
