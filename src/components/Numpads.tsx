999ad0f5d39564139384f2c2244e80abc57cdf9c
Yebin Yu <yuyaebean@gmail.com>
2021-03-28 23:29:35 +0900

    Feat: Set fonts and colors

--- src/components/Numpads.tsx
+++ src/components/Numpads.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 
 interface ButtonProps {
+  background?: string;
   column?: number;
 }
 
@@ -16,6 +17,7 @@ const Self = styled.div`
 
 const Button = styled.button<ButtonProps>`
   font-size: 18px;
+  background: ${props => (props.background ? props.background : '#333333')};
   width: ${props =>
     props.column ? props.column * 64 + (props.column - 1) * 12 + 'px' : '64px'};
   height: 64px;
@@ -23,28 +25,31 @@ const Button = styled.button<ButtonProps>`
   padding: 0;
   border-radius: 32px;
 
+  color: ${props => (props.color ? props.color : 'inherit')};
   grid-column: span ${props => props.column || 1};
 `;
 
 function Numpads() {
   return (
     <Self>
-      <Button column={4}>Clear</Button>
+      <Button background={'#a5a5a5'} color='#000' column={4}>
+        Clear
+      </Button>
       <Button>7</Button>
       <Button>8</Button>
       <Button>9</Button>
-      <Button>/</Button>
+      <Button background={'#fd9f07'}>/</Button>
       <Button>4</Button>
       <Button>5</Button>
       <Button>6</Button>
-      <Button>*</Button>
+      <Button background={'#fd9f07'}>*</Button>
       <Button>1</Button>
       <Button>2</Button>
       <Button>3</Button>
-      <Button>-</Button>
+      <Button background={'#fd9f07'}>-</Button>
       <Button column={2}>0</Button>
-      <Button>=</Button>
-      <Button>+</Button>
+      <Button background={'#fd9f07'}>=</Button>
+      <Button background={'#fd9f07'}>+</Button>
     </Self>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import GlobalStyles from './GlobalStyles';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
